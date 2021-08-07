2e8c84d3f8abb2edfd504ee3778a7442901a5a98
Anthony-ui <52468607+Anthony-ui@users.noreply.github.com>
2021-08-07 01:47:22 -0500

    holaCategoria

--- SAPMochaApi/Models/Categorias.cs
+++ SAPMochaApi/Models/Categorias.cs
@@ -8,6 +8,8 @@ namespace SapMochaApi.Models
 {
     public class Categorias
     {
+        
+        //HOLA
         [Key]
         public int IdCategorias { get; set; }
         public string Nombre { get; set; }
